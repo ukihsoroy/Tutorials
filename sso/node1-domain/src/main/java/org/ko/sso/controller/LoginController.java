46f70bf103fb16dedb23381d946e1c8aef6893f3
Artister <you_leet@foxmail.com>
2018-03-05 23:50:37 +0800

    #sso

---
+++ sso/node1-domain/src/main/java/org/ko/sso/controller/LoginController.java
@@ -0,0 +1,13 @@
+package org.ko.sso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping
+    public String index () {
+        return "login";
+    }
+}
