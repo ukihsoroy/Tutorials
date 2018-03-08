a80d026eade94eede6e0cf8dbb6e1b12fde9ce01
Artister <you_leet@foxmail.com>
2018-03-08 23:49:13 +0800

    #apache-poi

--- apache-poi/src/test/java/org/ko/poi/excel/ExcelIntegrationTest.java
+++ apache-poi/src/test/java/org/ko/poi/excel/ExcelIntegrationTest.java
@@ -1,9 +1,12 @@
 package org.ko.poi.excel;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.ko.poi.excel.mock.City;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExcelIntegrationTest {
 
-    private static String FILE_NAME = "project.xlsx";
+    private static String FILE_NAME = "city.xlsx";
+
     private String fileLocation;
 
     @Test
@@ -23,15 +27,29 @@ public class ExcelIntegrationTest {
         fileLocation = path.substring(0, path.length() - 1) + FILE_NAME;
         List<String> header = Arrays.asList(
                 "序号",
-                "姓名",
-                "年龄",
-                "生日",
-                "爱好",
-                "技能",
-                "婚否",
-                "月收入"
+                "主键",
+                "城市路径",
+                "城市名称",
+                "简写",
+                "创建时间"
         );
-        ExcelHelper.export();
+
+        //mock data
+        List<City> cities = readJson();
+        List<List<String>> rows = new ArrayList<>();
+        for (int i = 0; i < cities.size(); i++) {
+            City city = cities.get(i);
+            List<String> row = new ArrayList<>();
+            row.add(String.valueOf(i + 1));
+            row.add(String.valueOf(city.getId()));
+            row.add(city.getCityPath());
+            row.add(city.getCity());
+            row.add(city.getShorten());
+            row.add(ExcelHelper.formatDateTime(city.getCreateDate()));
+            rows.add(row);
+        }
+
+        ExcelHelper.export(header, rows, FILE_NAME);
 
     }
 
@@ -56,5 +74,15 @@ public class ExcelIntegrationTest {
         if (testFile.exists()) {
            testFile.delete();     
         }
+    }
+
+    private List<City> readJson () {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(new File("city.json"), mapper.getTypeFactory().constructParametricType(List.class, City.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
