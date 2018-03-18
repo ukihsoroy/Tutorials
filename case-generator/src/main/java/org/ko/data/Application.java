17f97a97afb007fc02cbc3a6372943df0f07067c
Artister <you_leet@foxmail.com>
2018-03-18 22:59:26 +0800

    #case-generator

--- case-generator/src/main/java/org/ko/data/Application.java
+++ case-generator/src/main/java/org/ko/data/Application.java
@@ -1,9 +1,11 @@
 package org.ko.data;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "org.ko.data.dao")
 public class Application {
 
     public static void main(String[] args) {
--- case-generator/src/main/java/org/ko/data/dao/LinkRepository.java
+++ case-generator/src/main/java/org/ko/data/dao/LinkRepository.java
@@ -1,6 +1,5 @@
-package org.ko.data.repository;
+package org.ko.data.dao;
 
-import org.ko.data.dao.LinkMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
