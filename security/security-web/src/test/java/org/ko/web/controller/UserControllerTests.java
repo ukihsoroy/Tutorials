ef8f9cd06e560ea1c1c69d9fd5f2202526e7ad94
Artister <you_leet@foxmail.com>
2018-04-09 00:58:43 +0800

    #security

---
+++ security/security-web/src/test/java/org/ko/web/controller/UserControllerTests.java
@@ -0,0 +1,46 @@
+package org.ko.web.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+//用SpringRunner来运行测试用例
+@RunWith(SpringRunner.class)
+public class UserControllerTests {
+
+    @Autowired private WebApplicationContext context;
+
+    private MockMvc mock;
+
+    @Before
+    public void setup () {
+        mock = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void whenQuerySuccess () throws Exception {
+        mock.perform(get("/user")
+                .param("username", "K.O")
+                .param("age", "18")
+                .param("ageTo", "20")
+//                .param("size", "15")
+//                .param("page", "3")
+//                .param("sort", "age,desc")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3));
+    }
+}
