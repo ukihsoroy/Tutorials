8be47e061a5ca33ef5f7f3a992fe7d634138f677
Artister <you_leet@foxmail.com>
2018-05-01 22:32:21 +0800

    #data-structure

---
+++ data-structure/src/test/java/org/ko/structure/map/MapTests.java
@@ -0,0 +1,16 @@
+package org.ko.structure.map;
+
+import org.junit.Test;
+
+public class MapTests {
+
+    @Test
+    public void testMapPutAndGet () {
+        KOMap<String, String> ko = new KOHashMap<>();
+        ko.put("1", "222");
+        ko.put("3", "111");
+        System.out.println(ko.get("1"));
+        System.out.println(ko.get("3"));
+    }
+
+}
