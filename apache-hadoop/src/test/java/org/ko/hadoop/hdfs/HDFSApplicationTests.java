06aad1e92ba9bcc2c0c0160d0c329196b71c5e8f
uleetu <you_leet@foxmail.com>
2018-05-31 23:16:14 +0800

    #apache-hadoop

--- apache-hadoop/src/test/java/org/ko/hadoop/hdfs/HDFSApplicationTests.java
+++ apache-hadoop/src/test/java/org/ko/hadoop/hdfs/HDFSApplicationTests.java
@@ -1,6 +1,7 @@
 package org.ko.hadoop.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.junit.jupiter.api.*;
@@ -21,23 +22,49 @@ class HDFSApplicationTests {
 
     private Configuration configuration = null;
 
-
+    /**
+     * <p>初始化HDFS</p>
+     * @throws Exception
+     */
     @BeforeEach void setUp () throws Exception {
         _LOGGER.info("HDFSApplicationTests#setUp");
         configuration = new Configuration();
-        configuration.set("fs.defaultFS", "hdfs://192.168.58.128:9000");
-        configuration.set("fs.hdfs.impl","org.apache.hadoop.hdfs.DistributedFileSystem");
-        configuration.set("dfs.client.block.write.replace-datanode-on-failure.policy", "NEVER");
-        fileSystem = FileSystem.get(configuration);
+        URI uri = new URI("hdfs://192.168.58.128:8020");
+//        fileSystem = FileSystem.get(uri, configuration, "K.O");
+//        configuration.set("fs.defaultFS", "hdfs://192.168.58.128:8020");
+//        configuration.set("fs.hdfs.impl","org.apache.hadoop.hdfs.DistributedFileSystem");
+//        configuration.set("dfs.client.block.write.replace-datanode-on-failure.policy", "NEVER");
+        fileSystem = FileSystem.get(uri, configuration, "K.O");
     }
 
+    /**
+     * <p>注销HDFS</p>
+     * @throws Exception
+     */
     @AfterEach void tearDown () throws Exception {
         configuration = null;
         fileSystem = null;
         _LOGGER.info("HDFSApplicationTests#tearDown");
     }
 
+    /**
+     * <p>创建文件夹</p>
+     * @throws IOException
+     */
     @Test void mkdir () throws IOException {
         fileSystem.mkdirs(new Path("/hdfsapi/test"));
     }
+
+    /**
+     * <p>创建文件</p>
+     * @throws IOException
+     */
+    @Test void createFile () throws IOException {
+        FSDataOutputStream outputStream = fileSystem.create(new Path("/hdfsapi/test/a.txt"));
+        outputStream.write("Hello Hadoop!".getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+
+
 }
