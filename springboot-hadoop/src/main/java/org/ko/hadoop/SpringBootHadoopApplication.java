c5c108a8b4c6f196f81047466e650184333e0e7a
uleetu <you_leet@foxmail.com>
2018-06-22 00:00:40 +0800

    #springboot-hadoop

--- springboot-hadoop/src/main/java/org/ko/hadoop/SpringBootHadoopApplication.java
+++ springboot-hadoop/src/main/java/org/ko/hadoop/SpringBootHadoopApplication.java
@@ -1,3 +1,5 @@
+package org.ko.hadoop;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
