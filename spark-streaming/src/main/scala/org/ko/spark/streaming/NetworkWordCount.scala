90acf713ba012211b8b03331774a07eadd7950c3
uzies <ko.shen@hotmail.com>
2018-09-23 11:36:35 +0800

    #spark-streaming

--- spark-streaming/src/main/scala/org/ko/spark/streaming/NetworkWordCount.scala
+++ spark-streaming/src/main/scala/org/ko/spark/streaming/NetworkWordCount.scala
@@ -6,20 +6,19 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 /**
   * Spark Streaming 处理socket数据
   *
-  * 测试： nc
+  * 测试： nc -lk 6789
   */
 object NetworkWordCount {
 
   def main(args: Array[String]): Unit = {
     //1. 创建spark conf配置
     val sparkConf = new SparkConf()
-      .setMaster("local[2]")
+      .setMaster("local")
       .setAppName("NetworkWordCount")
 
     //2. 创建StreamingContext需要两个参数: SparkConf 和 batch interval
     val ssc = new StreamingContext(sparkConf, Seconds(5))
-
-    val lines = ssc.socketTextStream("localhost", 6789)
+    val lines = ssc.socketTextStream("192.168.37.128", 6789)
 
     val result = lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_)
 
