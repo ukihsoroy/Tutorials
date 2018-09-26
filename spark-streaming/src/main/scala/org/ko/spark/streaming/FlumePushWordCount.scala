76b2f4ad25c84c9a9644c410f734eb3aa71bf85c
uzies <ko.shen@hotmail.com>
2018-09-26 21:39:39 +0800

    #spark-streaming

--- spark-streaming/src/main/scala/org/ko/spark/streaming/FlumePushWordCount.scala
+++ spark-streaming/src/main/scala/org/ko/spark/streaming/FlumePushWordCount.scala
@@ -10,18 +10,25 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 object FlumePushWordCount {
 
   def main(args: Array[String]): Unit = {
+
+    if (args.length != 2) {
+      println("Usage: Flume push word count <hostname> <port>")
+      System.exit(1)
+    }
+
+    val Array(hostname, port) = args
+
     val sparkConf = new SparkConf().setMaster("local[2]").setAppName("FlumePushWordCount")
     val ssc = new StreamingContext(sparkConf, Seconds(5))
 
     //Todo... 如何使用spark streaming整合flume
-    val flumeStream = FlumeUtils.createStream(ssc, "0.0.0.0", 8089)
+    val flumeStream = FlumeUtils.createStream(ssc, hostname, port.toInt)
 
     flumeStream.map(x => new String(x.event.getBody.array()).trim)
         .flatMap(_.split(" "))
         .map((_, 1))
         .reduceByKey(_ + _)
         .print()
-
 
     ssc.start()
     ssc.awaitTermination()
