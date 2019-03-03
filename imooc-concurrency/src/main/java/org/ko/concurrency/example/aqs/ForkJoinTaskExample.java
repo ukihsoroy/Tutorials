8aaa468646199bb645f1f2c94586573000275afe
uzies <ko.shen@hotmail.com>
2019-03-03 21:31:09 +0800

    juc

--- imooc-concurrency/src/main/java/org/ko/concurrency/example/aqs/ForkJoinTaskExample.java
+++ imooc-concurrency/src/main/java/org/ko/concurrency/example/aqs/ForkJoinTaskExample.java
@@ -1,4 +1,10 @@
 package org.ko.concurrency.example.aqs;
 
-public class ForkJoinTaskExample {
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinTaskExample extends RecursiveTask<Integer> {
+    @Override
+    protected Integer compute() {
+        return null;
+    }
 }
