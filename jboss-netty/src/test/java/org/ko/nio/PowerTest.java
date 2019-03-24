45e0686dd2db81065d23c95b7707c0a7d7008a0d
uzies <ko.shen@hotmail.com>
2019-03-24 22:25:55 +0800

    #netty

--- jboss-netty/src/test/java/org/ko/nio/PowerTest.java
+++ jboss-netty/src/test/java/org/ko/nio/PowerTest.java
@@ -1,12 +1,6 @@
 package org.ko.nio;
 
-import org.junit.Test;
 
 public class PowerTest {
 
-    @Test
-    public void test1 () {
-        byte[] b = "辽B4445".getBytes();
-        System.out.println(b.length);
-    }
 }
