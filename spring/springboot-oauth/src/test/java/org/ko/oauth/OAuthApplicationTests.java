8d830386f9d3139ddceeab4665722f1f182ecddb
uzies <ko.shen@hotmail.com>
2019-11-17 18:39:38 +0800

    #oauth server

---
+++ spring/springboot-oauth/src/test/java/org/ko/oauth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ko.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
