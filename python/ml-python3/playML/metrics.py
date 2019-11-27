83793534b51c24f90632f475678beb59e8aa0bea
sigmaol <ko.shen@hotmail.com>
2019-11-27 22:32:50 +0800

    confusion mafrix

--- python/ml-python3/playML/metrics.py
+++ python/ml-python3/playML/metrics.py
@@ -1,7 +1,6 @@
 import numpy as np
 from math import sqrt
 
-
 # 准确度的得分
 def accuracy_score(y_true, y_predict):
     """计算y_true和y_predict之间的准确率"""
@@ -36,3 +35,50 @@ def mean_absolute_error(y_true, y_predict):
 def r2_score(y_true, y_predict):
     """计算y_true和y_predict之间的R Square"""
     return 1 - mean_squared_error(y_true, y_predict) / np.var(y_true)
+
+
+# True Negative
+def TN(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 0) * (y_predict == 0))
+
+# False Positive
+def FP(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 0) * (y_predict == 1))
+
+# False Negative
+def FN(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 1) * (y_predict == 0))
+
+# True Positive
+def TP(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 1) * (y_predict == 1))
+
+# 混淆矩阵
+def confusion_matrix(y_true, y_predict):
+    return np.array([
+        [TN(y_true, y_predict), FP(y_true, y_predict)],
+        [FN(y_true, y_predict), TP(y_true, y_predict)]
+    ])
+
+
+# 准确率
+def precision_score(y_true, y_predict):
+    tp = TP(y_true, y_predict)
+    fp = FP(y_true, y_predict)
+    try:
+        return tp / (tp + fp)
+    except:
+        return 0.0
+
+# 召回率
+def recall_score(y_true, y_predict):
+    tp = TP(y_true, y_predict)
+    fn = FN(y_true, y_predict)
+    try:
+        return tp / (tp + fn)
+    except:
+        return 0.0
