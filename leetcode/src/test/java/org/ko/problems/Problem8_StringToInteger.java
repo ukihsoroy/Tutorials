4102848fc5817b8558dd07ba4e4963d0cd14b378
K.O <ko.shen@hotmail.com>
2019-12-26 17:45:25 +0800

    #p8

--- leetcode/src/test/java/org/ko/problems/Problem8_StringToInteger.java
+++ leetcode/src/test/java/org/ko/problems/Problem8_StringToInteger.java
@@ -1,47 +1,73 @@
 package org.ko.problems;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Implement atoi to convert a string to an integer.
- * Hint: Carefully consider all possible input cases.
- *       If you want a challenge, please do not see below and ask yourself what are the possible input cases.
- * Notes: It is intended for this problem to be specified vaguely (ie, no given input specs).
- *        You are responsible to gather all the input requirements up front.
+ * 字符串转整数
  */
 public class Problem8_StringToInteger {
 
-    /**
-     * Requirements for atoi:
-
-     The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
-
-     The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
-
-     If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
-
-     If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
-     */
-
-    /**
-     * 实现atoi将字符串转换为整数。
-
-     提示：仔细考虑所有可能的输入案例。如果你想要一个挑战，请不要在下面看到问问自己什么是可能的输入案例。
-
-     注意：这是针对这个问题被隐含的指定的（即没有给定的输入规格）。您有责任预先收集所有输入要求。
-
-
-
-     atoi的要求：
-
-     该函数首先丢弃尽可能多的空白字符，直到找到第一个非空白字符。然后，从这个字符开始，采用一个可选的初始正号或负号，后面跟随尽可能多的数字，并将它们解释为数字值。
-
-     该字符串可以在形成整数的那些字符之后包含额外的字符，这些字符被忽略并且对该函数的行为没有影响。
-
-     如果str中的第一个非空白字符序列不是有效整数，或者如果由于str为空或仅包含空白字符而不存在此类序列，则不执行转换。
-
-     如果不能执行有效的转换，则返回零值。如果正确值超出了可表示值的范围，则返回INT_MAX（2147483647）或INT_MIN（-2147483648）
-     */
-
     public int myAtoi(String str) {
-        return 0;
+        str = str.trim();
+        if (str == null || str.length() == 0)
+            return 0;
+
+        char firstChar = str.charAt(0);
+        int sign = 1;
+        int start = 0;
+        long res = 0;
+        if (firstChar == '+') {
+            sign = 1;
+            start++;
+        } else if (firstChar == '-') {
+            sign = -1;
+            start++;
+        }
+
+        for (int i = start; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return (int) res * sign;
+            }
+            res = res * 10 + str.charAt(i) - '0';
+            if (sign == 1 && res > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            if (sign == -1 && res > Integer.MAX_VALUE)
+                return Integer.MIN_VALUE;
+        }
+        return (int) res * sign;
+    }
+
+    @Test public void test1() {
+        String s = "12314124123123123123";
+        int r = myAtoi(s);
+        System.out.println(r);
+    }
+
+    @Test public void test2() {
+        String s = " 1231412";
+        int r = myAtoi(s);
+        System.out.println(r);
+    }
+
+    @Test public void test3() {
+        String s = " -1231412";
+        int r = myAtoi(s);
+        System.out.println(r);
+    }
+
+    @Test public void test4() {
+        String s = " -1231412 asdasd";
+        int r = myAtoi(s);
+        System.out.println(r);
+    }
+
+    @Test public void test5() {
+        String s = " sa-1231412 asdasd";
+        int r = myAtoi(s);
+        System.out.println(r);
     }
 }
