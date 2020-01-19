8548f896c5bd3f12ed0999090d8ac9e6049213e8
sigmaol <ko.shen@hotmail.com>
2020-01-19 22:14:37 +0800

    #添加数据库与日志配置

---
+++ activiti/02-start-by-config/src/test/java/org/ko/activiti/config/ConfigHistoryLevelTests.java
@@ -0,0 +1,42 @@
+package org.ko.activiti.config;
+
+import org.activiti.engine.logging.LogMDC;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * MDC测试
+ */
+public class ConfigHistoryLevelTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigTests.class);
+
+    /**
+     * 1. 不写参数，使用默认的
+     * 2. activiti_mdc.cfg.xml，添加了mdc的拦截器，打印树
+     */
+    @Rule
+    public ActivitiRule activitiRule = new ActivitiRule("activiti_mdc.cfg.xml");
+
+    @Test
+    //辅助单元测试的注解
+    @Deployment(resources = {"org.ko.activiti/my-process.bpmn20.xml"})
+    public void test() {
+        ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("my-process");
+        Task task = activitiRule.getTaskService().createTaskQuery().singleResult();
+        assertEquals("Activiti is awesome!", task.getName());
+        activitiRule.getTaskService().complete(task.getId());
+
+        //启动流程，完成task
+    }
+
+}
