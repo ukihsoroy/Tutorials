ddc418932d29772e0b3f8c65521fcdf84e5bf7af
sigmaol <ko.shen@hotmail.com>
2020-04-08 09:05:54 +0800

    #p14 - p16

--- leetcode/src/test/java/org/ko/problems/Problem15_ThreeSum.java
+++ leetcode/src/test/java/org/ko/problems/Problem15_ThreeSum.java
@@ -1,9 +1,6 @@
 package org.ko.problems;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem15_ThreeSum {
 
@@ -11,29 +8,29 @@ public class Problem15_ThreeSum {
 
     }
 
-    public List<List<Integer>> threeSum (int[] nums) {
-        //
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j ++) {
-                for (int k = j + 1; k < nums.length; k ++) {
-                    String node = nums[i] + "," + nums[j] + "," + nums[k];
-                    if (nums[i] + nums[j] + nums[k] == 0 && !set.contains(node)) {
-                        set.add(node);
-                    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        if(nums == null || len < 3) return ans;
+        Arrays.sort(nums); // 排序
+        for (int i = 0; i < len ; i++) {
+            if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
+            int L = i+1;
+            int R = len-1;
+            while(L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L<R && nums[L] == nums[L+1]) L++; // 去重
+                    while (L<R && nums[R] == nums[R-1]) R--; // 去重
+                    L++;
+                    R--;
                 }
+                else if (sum < 0) L++;
+                else if (sum > 0) R--;
             }
         }
-        List<List<Integer>> result = new ArrayList<>();
-        int s = 0;
-        for (String node : set) {
-            List<Integer> r = new ArrayList<>();
-            for (String n : node.split(",")) {
-                r.add(Integer.parseInt(n));
-            }
-            result.add(r);
-            s ++;
-        }
-        return result;
+        return ans;
     }
 }
