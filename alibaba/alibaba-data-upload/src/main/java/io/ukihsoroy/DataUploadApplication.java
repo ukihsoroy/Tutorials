9222be868a239844901fe5ead70b245e27cf1965
ukihsoroy <ko.shen@hotmail.com>
2020-10-17 22:31:14 +0800

    #alibaba-data-upload

--- alibaba/alibaba-data-upload/src/main/java/io/ukihsoroy/DataUploadApplication.java
+++ alibaba/alibaba-data-upload/src/main/java/io/ukihsoroy/DataUploadApplication.java
@@ -1,9 +1,7 @@
 package io.ukihsoroy;
 
-import io.ukihsoroy.automation.properties.AutomationDataSourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
