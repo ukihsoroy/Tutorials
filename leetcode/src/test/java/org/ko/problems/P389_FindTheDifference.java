5af41e5f099d4397e2200f1977433c147c28d621
ukihsoroy <ko.shen@hotmail.com>
2020-12-18 23:57:41 +0800

    #389，找不同

---
+++ leetcode/src/test/java/org/ko/problems/P389_FindTheDifference.java
@@ -0,0 +1,11 @@
+package org.ko.problems;
+
+public class P389_FindTheDifference {
+
+    public char findTheDifference(String s, String t) {
+        char[] chars = s.toCharArray();
+        char[] chars1 = t.toCharArray();
+
+
+    }
+}
