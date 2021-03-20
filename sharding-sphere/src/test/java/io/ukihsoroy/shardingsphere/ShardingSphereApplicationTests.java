a19627ec57d9858a110fba92c899547131b81058
ukihsoroy <ko.shen@hotmail.com>
2021-03-20 11:13:20 +0800

    init sharding-sphere

---
+++ sharding-sphere/src/test/java/io/ukihsoroy/shardingsphere/ShardingSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ukihsoroy.shardingsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
