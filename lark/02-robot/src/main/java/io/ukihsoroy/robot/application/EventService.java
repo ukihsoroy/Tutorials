68eea35c8109f0e1595bcd2b9553d024736aebdf
ukihsoroy <ko.shen@hotmail.com>
2021-07-13 00:01:32 +0800

    #commit

--- lark/02-robot/src/main/java/io/ukihsoroy/robot/application/EventService.java
+++ lark/02-robot/src/main/java/io/ukihsoroy/robot/application/EventService.java
@@ -1,9 +1,14 @@
 package io.ukihsoroy.robot.application;
 
-import javax.xml.ws.Response;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.ukihsoroy.robot.event.IEvent;
+import io.ukihsoroy.robot.event.IEventMessage;
 
 /**
  * <p></p>
@@ -14,9 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventService {
 
+    @Autowired private Map<String, IEvent<IEventMessage>> events;
+
     @PostMapping
-    public String event() {
-        return "";
+    public String event(@RequestBody String request) {
+        System.out.println(request);
+        return "1";
     }
 
 }
---
+++ lark/02-robot/src/main/java/io/ukihsoroy/robot/properties/RobotProperties.java
@@ -0,0 +1,10 @@
+package io.ukihsoroy.robot.properties;
+
+/**
+ * <p></p>
+ *
+ * @author K.O
+ * @email ko.shen@hotmail.com
+ */
+public class RobotProperties {
+}
