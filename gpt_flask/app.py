73458d08f1a91c7b92fa31b5e28dceaad3c4a929
ukihsoroy <ko.shen@hotmail.com>
2023-05-12 17:29:06 +0800

    #gpt调试

--- gpt_flask/app.py
+++ gpt_flask/app.py
@@ -2,38 +2,62 @@ import flask
 from flask import Flask
 import openai
 
-openai.api_key  = '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'
+openai.api_key = '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'
 
 app = Flask(__name__)
 
 
-@app.route('/query', methods=['post'])
-def index():
+@app.route('/<string:prompt_type>', methods=['post'])
+def index(prompt_type):
+    # 校验token，鉴权
     token = flask.request.headers.get("token")
-    print(token)
-    if token != "":
+    print(prompt_type)
+    if token != "68DFC3A7-4031-42B1-334B8A1B3982":
         return {
             "code": 0,
             "msg": "token error."
         }
-    body = flask.request.get_json()
-    prompt = f"""
-    By chinese identify a list of emotions that the writer of the \
-    following review is expressing. Include no more than \
-    five items in the list. Format your answer as a list of \
-    lower-case words separated by commas.
-    
-    Review: ```{body['prod_review']}```
-    """
 
+    # 获取review
+    body = flask.request.get_json()
+    review = body['prod_review']
+    if len(review) > 3500:
+        review = review[0:3500]
+
+    prompt = get_prompt(prompt_type, review)
+
+    # gpt调用
     res = get_completion(prompt)
     print(res)
+
     response = {
         "code": 0,
         "msg": "success",
         "response": res
     }
     return response
+
+
+def get_prompt(prompt_type, review):
+    prompts = {
+        "summarize": f"""
+            Your task is to generate a short summary in Chinese to feed back to the consumers of the solution.
+            To summarize the following solution,, delimited by triple 
+            backticks, in at most 30 words, and focusing on any aspects \
+            that mention shipping and delivery of the product. 
+            Please return in Chinese.
+            Review: ```{review}```
+            """,
+        "identify": f"""
+            Identify the industry attributes of the solution in Chinese. 
+            Include no more than five items in the list. Format your answer as a list of \
+            lower-case words separated by commas.
+            Please return in Chinese.
+            Review: ```{review}```
+            """
+    }
+
+    return prompts.get(prompt_type, None)
 
 
 def get_completion(prompt, model="gpt-3.5-turbo"): # Andrew mentioned that the prompt/ completion paradigm is preferable for this class
